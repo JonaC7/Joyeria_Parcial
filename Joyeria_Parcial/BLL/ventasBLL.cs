7fc1c65dd21d1c2889cfd38125af2ee318a37fc4
carri <carri@BAYRON>
2022-10-18 18:54:56 -0600

    Agregar archivos de proyecto.

---
+++ Joyeria_Parcial/BLL/ventasBLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joyeria_Parcial.BLL
+{
+    class ventasBLL
+    {
+    }
+}
